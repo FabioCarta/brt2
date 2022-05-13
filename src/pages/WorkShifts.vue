b9cfb1942e1c9d4f6ca135bba656cfc3de3d35d9
Thanh Phong Pham (ai1811) <thanh_phong.pham@smail.th-koeln.de>
2022-05-13 17:47:10 +0200

    fixed

--- src/pages/WorkShifts.vue
+++ src/pages/WorkShifts.vue
@@ -3,6 +3,7 @@
     <h2>Termine eintragen</h2>
     <h6>Beispiele von erlaubten Formaten:</h6>
     <p>mo6 di8 do15 sa10</p>
+    <p>modidosa10 (alle Uhrzeiten gleich)</p>
     <date-time-picker @add-to-dates="addToDates"></date-time-picker>
     {{ shiftsArray }}
   </q-page>
@@ -18,10 +19,24 @@ export default defineComponent({
     const shiftsArray = ref([]);
 
     const addToDates = (dates) => {
-      const datesArray = dates.split(' ');
-      datesArray.forEach((element) => {
-        shiftsArray.value.push(element);
-      });
+      const lastChar = dates.charAt(dates.length - 1);
+
+      const days = dates.substring(0, dates.indexOf(lastChar));
+
+      if (typeof parseInt(lastChar) === 'number' && !dates.includes(' ')) {
+        const datesArray = [];
+        for (let i = 0, charsLength = days.length; i < charsLength; i += 2) {
+          datesArray.push(days.substring(i, i + 2).concat(lastChar));
+        }
+        datesArray.forEach((element) => {
+          shiftsArray.value.push(element);
+        });
+      } else {
+        const datesArray = dates.split(' ');
+        datesArray.forEach((element) => {
+          shiftsArray.value.push(element);
+        });
+      }
     };
 
     return { shiftsArray, addToDates };
